6a4c2c4bee2ef0e7321a6d8f8ecd1f579a476c72
egaver1 <egaver1@umbc.edu>
2025-10-20 17:55:20 -0400

    Added school_map.js to load nodes and edges as a graph

--- js/mapSetup.js
+++ js/mapSetup.js
@@ -6,7 +6,7 @@ const nav_map = L.map("map").setView([lat, long], zoom_lvl);
 
 // {z}, {x}, {y} are correct, leaflet will substitute them with the correct coords based on the longitude and lat I specified
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {attribution: '&copy; OpenStreetMap contributors'}).addTo(nav_map); // We need to attribute OSM per their license
-
+/*
 let blueDot; // marker ref
 
 function placeBlueDot(lat, lng) {
@@ -50,3 +50,4 @@ if ("geolocation" in navigator) {
 } else {
   console.warn("Geolocation not supported in this browser.");
 }
+*/
